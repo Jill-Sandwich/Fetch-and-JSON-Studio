ccbfc326a7828687ccffff248cdfc99629134d62
Katie Dessain <JillSandwich@example.com>
2021-07-26 20:55:46 -0500

    updated astronaut div dynamically with javaScript and did the bonus mission!

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(responseJson) {
+            let myAstronautDiv = document.getElementById("container");
+            let myAstroCountDiv = document.getElementById("count");
+            myAstroCountDiv.innerHTML += ` <br> Astronaut Count: ${responseJson.length}`;
+            let content = "";
+            responseJson.sort(function(astro1, astro2){return astro2.hoursInSpace - astro1.hoursInSpace});
+            for (let index = 0; index < responseJson.length; index++) {
+                let activeGreen = "";
+                if (responseJson[index].active === true){
+                    activeGreen = "activeGreenText";
+                }
+                content += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${responseJson[index].firstName} ${responseJson[index].lastName}</h3>
+                        <ul>
+                            <li>Hours in Space: ${responseJson[index].hoursInSpace}</li>
+                            <li class="${activeGreen}">Active: ${responseJson[index].active}</li>
+                            <li>Skills: ${responseJson[index].skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${responseJson[index].picture}">
+                </div>
+                `
+            }
+            myAstronautDiv.innerHTML = content;
+        });
+    });
+});
